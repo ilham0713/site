6f46df35ac68f07ecfef3bda16138824e689257f
Ilham Mukati <ilhammukati@gmail.com>
2023-08-05 16:06:38 -0700

    just confirming

--- src/components/expertise/Expertise.jsx
+++ src/components/expertise/Expertise.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './expertise.css'
+import { HiDesktopComputer } from 'react-icons/hi';
+import { BsFillClipboardCheckFill } from 'react-icons/bs';
+import { DiReact } from 'react-icons/di';
+
 
 const Expertise = () => {
     return (
@@ -10,19 +14,30 @@ const Expertise = () => {
             <h2>
                 What am I good at?
             </h2>
-            <div className="container table">
-                <div>
-                    Backend
+            <div className="expert-content">
+                <div className = "about-cards">
+                    <article classname = "about_card">
+                        <HiDesktopComputer className='about-icon'/>
+                        <h5>Back-End</h5>
+                        <small>3+ years</small>
+                    </article>
+                    <article classname = "about_card">
+                        <DiReact className='about-icon'/>
+                        <h5>Front-End</h5>
+                        <small>2+ years</small>
+                        {/* <p>dhsfihwfhiwhfwehuwehuiewhfeuiwhfuwehfuwehfuewhfwjehwfucehwnufewhfihwfwjdhfuwehfuwehuewjuwjewjuewufuwejfewf
+                            fehwfhuewhfuwehfeefhfjewhfnhiwbfwe
+                        </p> */}
+                    </article>
+                    <article classname = "about_card">
+                        <BsFillClipboardCheckFill className='about-icon'/>
+                        <h5>Frameworks and Applications</h5>
+                        <small>3+ years</small>
+                    </article>
                 </div>
-                <div>
-                    Frontend
-                </div>
-
             </div>
+
             
-            {/* <div>
-                Applications/Frameworks
-            </div> */}
         </section>
     )
 }
